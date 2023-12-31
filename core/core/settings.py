fbdae32578cd5e30549cf034be643b69d3614ae9
budeikin <budeikin52@gmail.com>
2023-12-31 17:18:02 +0330

    adding bookmark it with js

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    'django_extensions'
+    'django_extensions',
+    'images.apps.ImagesConfig'
 ]
 
 MIDDLEWARE = [
